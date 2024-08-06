36fcf109c01866e854af8e3a0f438cac6570e562
PRABHURAM VENKATESAN <prabhuram.v@gmail.com>
2024-08-06 15:50:28 -0400

    updated dissect_from_dir to have value parameters rather than dictionaries as parameters.

--- smart_tools/dissector/dirdissector.py
+++ smart_tools/dissector/dirdissector.py
@@ -6,13 +6,13 @@ import pandas as pd
 from smart_tools.dissector.filedissector import dissect_from_file
 
 
-def dissect_from_dir(args, configs):
-    files = glob.glob(os.path.join(args['dir'], args['file']))
+def dissect_from_dir(dir, file, sep, nsample=10, slicers=[''], skiprows=0, skipfooter=0, colnames=None):
+    files = glob.glob(os.path.join(dir, file))
     df_all = []
     print(f'files: {len(files)}')
     for file in files:
-        for d in dissect_from_file(file, sep=args['sep'], nsample=configs['nsample'], slicers=args['slicers'],
-                                   skiprows=0, skipfooter=0, names=args['cols']):
+        for d in dissect_from_file(file, sep=sep, nsample=nsample, slicers=slicers,
+                                   skiprows=skiprows, skipfooter=skipfooter, names=colnames):
             df_all.append(d)
     df_all = concat_dfs(df_all)
     return df_all
