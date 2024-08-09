083c5dbb294c14f00d8e7438d479f74d6755414e
PRABHURAM VENKATESAN <prabhuram.v@gmail.com>
2024-08-09 03:37:30 -0400

    added `long_description` to setup.

--- setup.py
+++ setup.py
@@ -1,11 +1,19 @@
 from setuptools import setup, find_packages
 
+# read the contents of your README file
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+
 setup(
     name='smart_tools',
     version='0.5',
     description='A variety of smart tools to make analytics easy.',
     author='Prabhuram Venkatesan',
     readme = "README.md",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     packages=find_packages(),
     entry_points={
         'console_scripts': [
@@ -23,4 +31,8 @@ setup(
         ('config', ['config/dissector_config.yaml', 'config/iban_config.yaml']),
     ],
     python_requires='>=3.8',
+    project_urls = {
+        'Home': 'https://pypi.org/project/smart-tools/',
+        'Github': 'https://github.com/arcot23/smart_tools',
+    },
 )
