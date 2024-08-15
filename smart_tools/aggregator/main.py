a7cdcc39524c5cb12065662c67fbc4b3e1121616
PRABHURAM VENKATESAN <prabhuram.v@gmail.com>
2024-08-15 16:01:41 -0400

    added config file to aggregator

--- smart_tools/aggregator/main.py
+++ smart_tools/aggregator/main.py
@@ -1,5 +1,6 @@
 import argparse
 import os
+import yaml
 from smart_tools.aggregator import fileaggregator
 
 def main():
@@ -11,16 +12,32 @@ def main():
     parser.add_argument('--to', choices=['xlsx', 'json', 'csv'], default='xlsx',
                         help='Save result to xlsx or json or csv (default: `xlsx`)')
     parser.add_argument('--outfile', type= str, default='.\.a\\aggregated_result',
-                        help='Output directory and file name (default: .\dissect_result)')
+                        help='Output directory and file name (default: .\a\\aggregated_result)')
+    parser.add_argument('--config', default='.\config\\aggregator_config.yaml',
+                        help='Config file for meta data (default: `.\config\\aggregator_config.yaml`)')
 
     args = vars(parser.parse_args())
+
+    if not os.path.isabs(args['config']): args['config'] = os.path.join(os.path.dirname(os.path.realpath(__file__)),
+                                                                        args['config'])
+
+    if not os.path.exists(args['config']):
+        print(f"Config file `{args['config']}` missing.")
+        return 1
 
     print('**Arguments:**')
     for arg in args:
         print(f'- {arg}: `{args[arg]}`')
 
+    with open(args['config'], 'r') as f:
+        configs = yaml.safe_load(f)
+
+    print('**Config:**')
+    for config in configs:
+        print(f'- {config}: `{configs[config]}`')
+
     print('**Process:**')
-    df_all = fileaggregator.aggregate_files(dir=args['dir'], file_wildcard=args['file'], sep=args['sep'],include_filename=True)
+    df_all = fileaggregator.aggregate_files(dir=args['dir'], file_wildcard=args['file'], sep=args['sep'],include_filename=True, **configs['read_csv'])
 
     print('**Result:**')
     output_path = os.path.join(args['dir'], f'{args["outfile"]}.{args["to"]}')
