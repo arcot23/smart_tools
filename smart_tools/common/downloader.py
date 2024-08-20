465c2b8b3c5a04d2d229733b72123b03db86aaf0
PRABHURAM VENKATESAN <prabhuram.v@gmail.com>
2024-08-20 17:02:57 -0400

    bug fix

--- smart_tools/common/downloader.py
+++ smart_tools/common/downloader.py
@@ -8,7 +8,7 @@ def get_url(url, http_proxy ='http://proxy:8080', https_proxy ='http://username:
     if https_proxy:
         proxy['https'] = https_proxy
 
-    return requests.get(url, proxy)
+    return requests.get(url, proxies= proxy)
 
 def download_url(url, filename, encoding ='utf-8', http_proxy ='http://proxy:8080', https_proxy ='http://username:pwd@proxy:8080'):
     response = get_url(url, http_proxy, https_proxy)
