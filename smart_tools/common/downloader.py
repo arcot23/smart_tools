c98c10ecf1707a231ec69c1203c84c5afb0fb65a
PRABHURAM VENKATESAN <prabhuram.v@gmail.com>
2024-09-17 15:13:57 -0400

    renamed function and signatures.

--- smart_tools/common/downloader.py
+++ smart_tools/common/downloader.py
@@ -1,6 +1,6 @@
 import requests
 
-def get_url(url, http_proxy ='http://proxy:8080', https_proxy ='http://username:pwd@proxy:8080'):
+def request_url(url, http_proxy ='http://proxy:8080', https_proxy ='http://username:pwd@proxy:8080'):
 
     """
     Gets a Url and returns the `Response`.
@@ -18,18 +18,18 @@ def get_url(url, http_proxy ='http://proxy:8080', https_proxy ='http://username:
 
     return requests.get(url, proxies= proxy)
 
-def download_url(url, filename, encoding ='utf-8', http_proxy ='http://proxy:8080', https_proxy ='http://username:pwd@proxy:8080'):
+def download_url_to_file(url, filename, encoding = None, http_proxy = None, https_proxy = None):
 
     """
     Download content of the url to a file.
     :param url: Url.
     :param filename: Local filename to save the content to.
-    :param encoding: Encoding of the `filename`.
-    :param http_proxy: Http proxy address if one is needed.
-    :param https_proxy: Https proxy address if one is needed.
+    :param encoding: Encoding of the `filename`. e.g., 'utf-8'
+    :param http_proxy: Http proxy address if one is needed. e.g., 'http://proxy:8080'
+    :param https_proxy: Https proxy address if one is needed. e.g., 'http://username:pwd@proxy:8080'
     :return: IO
     """
-    response = get_url(url, http_proxy, https_proxy)
+    response = request_url(url, http_proxy, https_proxy)
 
     with open(filename, 'wb', encoding=encoding) as f:
         f.write(response.content)
