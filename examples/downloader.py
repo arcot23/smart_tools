ae33b44f082d8284fb78dbf1bedbbf59224c2722
PRABHURAM VENKATESAN <prabhuram.v@gmail.com>
2024-09-17 15:14:13 -0400

    added an example.

---
+++ examples/downloader.py
@@ -0,0 +1,6 @@
+from smart_tools.common import downloader
+
+downloader.download_url_to_file(
+    url=r"https://sanctionslistservice.ofac.treas.gov/api/PublicationPreview/exports/SDN_ENHANCED.ZIP",
+    filename=r"C:\Users\prabhuramv\Downloads\sdn.zip",
+)
